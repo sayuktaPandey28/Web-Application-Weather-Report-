f8c172c5e6095ad64cb51cbe9a62d79a8fed10df
Sayukta Pandey <131333555+sayuktaPandey28@users.noreply.github.com>
2024-07-27 11:56:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Which planet has the most moons?",
+        a: "Earth",
+        b: "Saturn",
+        c: "Jupiter",
+        d: "Mars",
+        correct: "b",
+    },
+    {
+        question: "What is the capital of Japan?",
+        a: "Beijing",
+        b: "Tokyo",
+        c: "Seoul",
+        d: "Bangkok",
+        correct: "b",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Cascading Style Sheets",
+        b: "Central Style Sheet",
+        c: "Cars SUVs Sailboats",
+        d: "Cascading Simple Sheets",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "1995",
+        correct: "b",
+    }
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+function getSelected() {
+    let answer;
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
